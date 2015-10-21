c1ce62182f2fe17f177aeb9598a0b756f818feb6
unknown <ocouls01@CSPC123.dcs.bbk.ac.uk>
2015-10-21 13:37:21 +0100

    Started circular list exercise, started cPatient class, need to write deletePatient method

---
+++ cPatient.java
@@ -0,0 +1,30 @@
+public class cPatient {
+	private String name;
+	private int age;
+	private String illness;
+	private cPatient next;
+	
+	public cPatient(String name, int age, String illness) {
+		this.name = name;
+		this.age = age;
+		this.illness = illness;
+		this.next = this;
+	}
+	
+	// add patient method
+	
+	public void addCPatient(cPatient start, cPatient newPatient) {
+		if (this.next == start) {
+			this.next = newPatient;
+			newPatient.next = start;
+		}
+		else {
+			this.next.addCPatient(start, newPatient);
+		}
+	}
+	
+	//delete patient method
+	public void deletePatient(cPatient start) {
+		
+	}
+}
